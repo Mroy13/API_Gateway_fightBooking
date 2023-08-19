6422fe6972ea3f6d5a158d3b5597939f57b89f15
Mroy13 <mtsprotechguide@gmail.com>
2023-08-19 20:40:09 +0530

    FEAT:Added reverse proxy for flightService and bookingService

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,5 +3,7 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     SALTROUNDS: process.env.SALT_ROUND,
-    SECRET_KEY: process.env.SECRET_KEY
+    SECRET_KEY: process.env.SECRET_KEY,
+    flight_service: process.env.FLIGHT_SERVICE,
+    booking_service: process.env.BOOKING_SERVICE
 }
