c4c373c9ec8185db2cbf991a71daca3fc8ba6da7
Mroy13 <mtsprotechguide@gmail.com>
2023-08-19 18:47:26 +0530

    FEAT:Added rate limiter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express=require('express');
- const routes=require('./routes');
+const routes=require('./routes');
+const rateLimit = require('express-rate-limit')
  const {ServerConfig}=require('./config');
  
 
  const app=express();
+ const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	max: 5, // Limit each IP to 5 requests per `window` (here, per 1 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false,
+})
+ app.use(limiter);
  app.use(express.json());
  app.use(express.urlencoded({extended: true}));
  app.use('/api',routes);
